ebe4d85a77f2cf6259243a08248da0882e37d6eb
drewbaine <139158590+drewbaine@users.noreply.github.com>
2025-09-18 12:28:40 -0400

    Update Project1.c
    initial. Sets up the loop

--- Project1.c
+++ Project1.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+int main()
+{
+ // use these variables
+pid_t pid, child, origin;
+ int status;
+ int n;
+ 
+printf("Processes ");
+scanf("%d", &n);
+origin = getpid();
+for(int i = 0; i < n; i++){
+pid = fork();
+ if (pid < 0) {
+ fprintf(stderr, "Fork failure: %s", strerror(errno));
+ exit(1);
+ }
+ else if(pid == 0 && i+1 == n){
+    printf("I am child PID %ld\n", (long) getpid());
+    child = getpid();
+    exit(0);
+ }
+ else if (pid == 0) {
+ printf("I am child PID %ld\n", (long) getpid());
+ child = getpid();
+ }
+ else {
+ waitpid(pid, &status, 0);
+
+printf("this processes pid: %ld Child PID %ld terminated with return status %d\n", (long)
+getpid(), (long) pid, status);
+break;
+ }
+}
+ return 0;
+}
 
